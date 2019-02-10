deaddcc19432d5db8c5382e2ebf049f22fe35d28
Tamas Balogh <Tamas.Balogh@logmein.com>
2019-02-10 15:25:31 +0100

    Implement LoginTest and RegistrationTest tests

--- app/src/androidTest/java/com/tbalogh/indigo/LoginTest.java
+++ app/src/androidTest/java/com/tbalogh/indigo/LoginTest.java
@@ -1,10 +1,15 @@
 package com.tbalogh.indigo;
 
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.assertion.ViewAssertions;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
 
 
 @RunWith(AndroidJUnit4.class)
@@ -24,20 +29,36 @@ public class LoginTest {
     @Test
     public void login_withEmptyEmailAndPassword_errorMessagesShown() throws InterruptedException {
         // Click the button login
+        onView(ViewMatchers.withId(R.id.loginButton))
+            .perform(ViewActions.click());
 
         // Assert that the email field and the password field has the error text: "Must be not empty!"
+        onView(ViewMatchers.withId(R.id.emailField))
+                .check(ViewAssertions.matches(ViewMatchers.hasErrorText("Must be not empty!")));
+        onView(ViewMatchers.withId(R.id.passwordField))
+                .check(ViewAssertions.matches(ViewMatchers.hasErrorText("Must be not empty!")));
     }
 
     /* 1.2
      *  Verify that the error message shown if wrong email and password was given
      * */
     @Test
-    public void login_withInvalidEmailAndPassword_errorMessagesShown() throws InterruptedException {
+    public void login_withInvalidCredentials_errorMessagesShown() throws InterruptedException {
         // Type the invalid credentials (use INVALID_EMAIL and INVALID PASSWORD variable)
+        onView(ViewMatchers.withId(R.id.emailField))
+                .perform(ViewActions.typeText(INVALID_EMAIL));
+        onView(ViewMatchers.withId(R.id.passwordField))
+                .perform(ViewActions.typeText(INVALID_PASSWORD));
 
         // Click the button login
+        onView(ViewMatchers.withId(R.id.loginButton))
+                .perform(ViewActions.click());
 
         // Assert that the email field and the password field has the error text: "Invalid credentials!" and "Invalid credentials!"
+        onView(ViewMatchers.withId(R.id.emailField))
+                .check(ViewAssertions.matches(ViewMatchers.hasErrorText("Invalid credentials!")));
+        onView(ViewMatchers.withId(R.id.passwordField))
+                .check(ViewAssertions.matches(ViewMatchers.hasErrorText("Invalid credentials!")));
     }
 
     /* 1.3
@@ -46,9 +67,17 @@ public class LoginTest {
     @Test
     public void login_withValidEmailAndPassword_openedTheAppPage() throws InterruptedException {
         // Type the valid credentials (use VALID_EMAIL and VALID PASSWORD variable)
+        onView(ViewMatchers.withId(R.id.emailField))
+                .perform(ViewActions.typeText(VALID_EMAIL));
+        onView(ViewMatchers.withId(R.id.passwordField))
+                .perform(ViewActions.typeText(VALID_PASSWORD));
 
         // Click the button login
+        onView(ViewMatchers.withId(R.id.loginButton))
+                .perform(ViewActions.click());
 
         // Assert that the "Welcome to the app!" message is displayed.
+        onView(ViewMatchers.withId(R.id.welcomeText))
+                .check(ViewAssertions.matches(ViewMatchers.withText("Welcome to the app!")));
     }
 }
--- app/src/androidTest/java/com/tbalogh/indigo/RegistrationTest.java
+++ app/src/androidTest/java/com/tbalogh/indigo/RegistrationTest.java
@@ -1,5 +1,8 @@
 package com.tbalogh.indigo;
 
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.assertion.ViewAssertions;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
 import org.junit.Before;
@@ -7,10 +10,17 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
+
 @RunWith(AndroidJUnit4.class)
 public class RegistrationTest {
 
     public static String SHORT_PASSWORD = "1234567";
+
+    public static String WRONG_FORMAT_EMAIL = "testlogmein.com";
+
+    public static String DIFFERENT_PASSWORD_1 = "password1";
+    public static String DIFFERENT_PASSWORD_2 = "password2";
 
     public static String VALID_EMAIL = "test@logmein.com";
     public static String VALID_PASSWORD = "password";
@@ -21,6 +31,8 @@ public class RegistrationTest {
     @Before
     public void setUp() throws Exception {
         // Navigate to the Registration view
+        onView(ViewMatchers.withId(R.id.openRegistrationButton))
+                .perform(ViewActions.click());
     }
 
     /* 2.1
@@ -29,8 +41,16 @@ public class RegistrationTest {
     @Test
     public void register_noFieldWasFilled_errorMessageShown() {
         // Click the button register
+        onView(ViewMatchers.withId(R.id.registerButton))
+                .perform(ViewActions.click());
 
         // Assert that fields has the error text: "Must be not empty!"
+        onView(ViewMatchers.withId(R.id.emailField))
+                .check(ViewAssertions.matches(ViewMatchers.hasErrorText("Must be not empty!")));
+        onView(ViewMatchers.withId(R.id.passwordField))
+                .check(ViewAssertions.matches(ViewMatchers.hasErrorText("Must be not empty!")));
+        onView(ViewMatchers.withId(R.id.confirmPasswordField))
+                .check(ViewAssertions.matches(ViewMatchers.hasErrorText("Must be not empty!")));
     }
 
     /* 2.2
@@ -38,11 +58,17 @@ public class RegistrationTest {
      * */
     @Test
     public void register_emailHasWrongFormat_errorMessageShown() {
-        // Fill the email field with wrong format
+        // Fill the email field with wrong format (use WRONG_FORMAT_EMAIL field)
+        onView(ViewMatchers.withId(R.id.emailField))
+                .perform(ViewActions.typeText(WRONG_FORMAT_EMAIL));
 
         // Click the button register
+        onView(ViewMatchers.withId(R.id.registerButton))
+                .perform(ViewActions.click());
 
-        // Assert that fields has the error text: "Invalid email address!"
+        // Assert that fields has the error text: "Invalid email format!"
+        onView(ViewMatchers.withId(R.id.emailField))
+                .check(ViewAssertions.matches(ViewMatchers.hasErrorText("Invalid email format!")));
     }
 
     /* 2.3
@@ -50,11 +76,21 @@ public class RegistrationTest {
      * */
     @Test
     public void register_passwordIsTooShort_errorMessageShown() {
-        // Fill the password fields with passwords with SHORT_PASSWORD
+        // Fill the password and confirmPassword fields with passwords with SHORT_PASSWORD
+        onView(ViewMatchers.withId(R.id.passwordField))
+                .perform(ViewActions.typeText(SHORT_PASSWORD));
+        onView(ViewMatchers.withId(R.id.confirmPasswordField))
+                .perform(ViewActions.typeText(SHORT_PASSWORD));
 
         // Click the button register
+        onView(ViewMatchers.withId(R.id.registerButton))
+                .perform(ViewActions.click());
 
         // Assert that fields has the error text: "Password must be at least 8 character!"
+        onView(ViewMatchers.withId(R.id.passwordField))
+                .check(ViewAssertions.matches(ViewMatchers.hasErrorText("Password must be at least 8 character!")));
+        onView(ViewMatchers.withId(R.id.confirmPasswordField))
+                .check(ViewAssertions.matches(ViewMatchers.hasErrorText("Password must be at least 8 character!")));
     }
 
     /* 2.4
@@ -62,11 +98,21 @@ public class RegistrationTest {
      * */
     @Test
     public void register_passwordsDoesNotMatch_errorMessageShown() {
-        // Fill the password fields with different values
+        // Fill the password fields with different values. Use DIFFERENT_PASSWORD_1 and DIFFERENT_PASSWORD_2 fields.
+        onView(ViewMatchers.withId(R.id.passwordField))
+                .perform(ViewActions.typeText(DIFFERENT_PASSWORD_1));
+        onView(ViewMatchers.withId(R.id.confirmPasswordField))
+                .perform(ViewActions.typeText(DIFFERENT_PASSWORD_2));
 
         // Click the button register
+        onView(ViewMatchers.withId(R.id.registerButton))
+                .perform(ViewActions.click());
 
         // Assert that fields has the error text: "Passwords does not match!"
+        onView(ViewMatchers.withId(R.id.passwordField))
+                .check(ViewAssertions.matches(ViewMatchers.hasErrorText("Passwords does not match!")));
+        onView(ViewMatchers.withId(R.id.confirmPasswordField))
+                .check(ViewAssertions.matches(ViewMatchers.hasErrorText("Passwords does not match!")));
     }
 
 
@@ -76,10 +122,20 @@ public class RegistrationTest {
     @Test
     public void register_validData_registrationSucceeded() {
         // Fill the email and password fields with the valid values
+        onView(ViewMatchers.withId(R.id.emailField))
+                .perform(ViewActions.typeText(VALID_EMAIL));
+        onView(ViewMatchers.withId(R.id.passwordField))
+                .perform(ViewActions.typeText(VALID_PASSWORD));
+        onView(ViewMatchers.withId(R.id.confirmPasswordField))
+                .perform(ViewActions.typeText(VALID_PASSWORD));
 
         // Click the button register
+        onView(ViewMatchers.withId(R.id.registerButton))
+                .perform(ViewActions.click());
 
-        // Assert that the "Registratiton was successful!" message appeared.
+        // Assert that the registrationSucceededView appeared.
+        onView(ViewMatchers.withId(R.id.registrationSucceededView))
+                .check(ViewAssertions.matches(ViewMatchers.withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
     }
 
 }
